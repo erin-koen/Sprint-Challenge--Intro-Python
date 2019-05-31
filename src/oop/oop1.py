93fa29ddd5688435aa2636d23127e1f16f484f94
Erin Koen <erin.koen@gmail.com>
2019-05-31 11:06:44 -0400

    oop1.py done

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -21,4 +21,20 @@
 class Vehicle:
     pass
 
+class FlightVehicle(Vehicle):
+    pass
 
+class GroundVehicle(Vehicle):
+    pass
+
+class Starship(FlightVehicle):
+    pass
+
+class Airplane(FlightVehicle):
+    pass
+
+class Car(GroundVehicle):
+    pass
+
+class Motorcycle(GroundVehicle):
+    pass
